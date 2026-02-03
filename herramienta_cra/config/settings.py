81a140223fe48240615af92bc49341966a6e6b09
AlvaroSanGar <alvaro.s.garijo@gmail.com>
2026-02-03 18:26:58 +0000

    eliminación de utils y settings provisional

---
+++ herramienta_cra/config/settings.py
@@ -0,0 +1,18 @@
+import yaml
+import sys # de momento lo dejamos pero luego se puede comprobar si funciona exit() normal
+
+
+def loadYaml(path):
+    try:
+        with open(path,"r", encoding='utf-8') as f:
+            return yaml.safe_load(f)
+    
+    except FileNotFoundError:
+        print('Error al cargar el yaml, archivo no encontrado\n')
+        sys.exit(1)
+    
+    except yaml.YAMLError:
+        print("El archivo yaml contiene errores\n")
+        sys.exit(1)
+        
+CONFIG = loadYaml("config/controls.yaml")
