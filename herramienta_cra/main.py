3542bc5ece8dd9f584ef81daed80e6ccb0f8cf83
AlvaroSGar <alvaro.s.garijo@gmail.com>
2026-02-09 12:34:43 +0100

    Recopilación de info básica y de los paquetes instalados para analizar las vulnerabilidades

--- herramienta_cra/main.py
+++ herramienta_cra/main.py
@@ -2,8 +2,29 @@ import modules.network as mNetwork
 import modules.system as mSystem
 
 if __name__ == "__main__":
-    print("--- [ FASE 1: AUDITORÍA DE PUERTOS ] ---")
+    print("\n==================================================")
+    print("      AUDITORÍA DE CUMPLIMIENTO CRA (LINUX)      ")
+    print("==================================================\n")
+    
+    datos_reporte = {
+        "sistema": {},
+        "paquetes": [],
+        "puertos": []
+    }
     verbose = True
+    
+    # 1. Info del Host
+    info_sis = mSystem.info_sis()
+    datos_reporte["sistema"] = info_sis 
+    if verbose:
+        print("[+] Información del sistema: ")
+        print("     [i] Hostname:      "+info_sis['hostname'])
+        print("     [i] Sistema:       "+info_sis['distro']+" "+info_sis['version'])
+        print("     [i] Kernel:        "+info_sis['kernel'])
+        print("     [i] Arquitectura:  "+info_sis['arquitectura']+"\n")
+    
+    
+    print("--- [ FASE 1: AUDITORÍA DE PUERTOS ] ---")
     interfaces = mSystem.obtener_interfaces_red()
     lista_objetivos = []
     
@@ -17,8 +38,31 @@ if __name__ == "__main__":
     if not lista_objetivos:
         print("[ERROR] No se detectaron IPs para escanear\n")
         exit(1)
-    resultadosNetwork = mNetwork.scaneoPuertos(lista_objetivos, verbose)
+    datos_reporte["puertos"] = mNetwork.scaneoPuertos(lista_objetivos, verbose)
     
+    
+    
+    print("\n--- [ FASE 2: INVENTARIO DEL SISTEMA ] ---")
+    print("[+] Iniciadno módulo de revisión de paquetes instalados")
+    paquetes = mSystem.paquetes_instalados()
+    datos_reporte["paquetes"] = paquetes # Guardamos para el PDF
+    
+    print("   [i] Total de paquetes detectados: "+str(len(paquetes)))
+    
+    # Ejemplo para verbose
+    if (len(paquetes) > 0) and verbose:
+        ejemplos = [f"{p['name']} v{p['version']}" for p in paquetes[:3]]
+        print(f"   [i] Ejemplos: {', '.join(ejemplos)}...")
+    elif verbose:
+        print("    [!] No se detectaron paquetes")
+
     print("\n\n--- [ FIN DEL ESCANEO ] ---\n")
-    print("Total de datos guardados: "+str(len(resultadosNetwork))+"\n")
-    print (resultadosNetwork)
+    print("RESULTADOS SISTEMA\n")
+    print(datos_reporte["sistema"])
+    print("\n\n\nRESULTADOS DE NETWORKING\n")
+    print(datos_reporte["puertos"])
+    print("\n\n\nRESULTADOS DE PAQUETES\n")
+    #print(datos_reporte["paquetes"])
+    
+    
+    
--- herramienta_cra/modules/system.py
+++ herramienta_cra/modules/system.py
@@ -1,7 +1,29 @@
 import subprocess
 import sys
+import json
+import platform
 from collections import defaultdict
 
+# Esta función sirve para realizar una consulta con OSqueryi
+def ejecutar_consulta(query):
+    try:
+        resultado = subprocess.run(
+            ["osqueryi", "--json", query],
+            capture_output=True,
+            text=True,
+            check=True
+        )
+        return json.loads(resultado.stdout)
+    
+    except FileNotFoundError:
+        print("[ERROR] No se ha encontrado osqueryi")
+        sys.exit(1)
+    except Exception as e:
+        print("[ERROR] Fallo al ejecutar consulta Osquery: "+str(e))
+        return []
+    
+    
+# Para el módulo de networking
 def obtener_interfaces_red():
     try:
         # ejecuta ip -o addr show y lo guardamos en formato stirng
@@ -31,3 +53,28 @@ def obtener_interfaces_red():
             })
             
     return dict(interfaces)
+
+
+# Obtener info básica
+def info_sis():
+    info = {
+        "hostname": platform.node(),
+        "sistema": platform.system(),
+        "distro": "Desconocida",
+        "version": "Desconocida",
+        "kernel": platform.release(),
+        "arquitectura": platform.machine()
+    }
+    data = ejecutar_consulta("SELECT name, version FROM os_version;")
+    if data and len(data) > 0:
+        info["distro"] = data[0].get("name", "Linux")
+        info["version"] = data[0].get("version", "")
+    return info
+
+
+
+# Obtener paquetes instalados
+def paquetes_instalados():
+    query = "SELECT name, version FROM deb_packages;"
+    resultado = ejecutar_consulta(query)
+    return resultado
