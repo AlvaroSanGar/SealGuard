1e64d4938a2b859ba8b9d8f7d5a53395e92da78c
AlvaroSGar <alvaro.s.garijo@gmail.com>
2026-03-05 09:13:29 +0100

    cambio en integrity

--- herramienta_cra/modules/integrity.py
+++ herramienta_cra/modules/integrity.py
@@ -3,12 +3,16 @@ import json
 import os
 from config.settings import config
 
-def calcular_hash(ruta):
+def calcular_integridad(ruta):
     # Comprobamos si existe
+    
     if not os.path.exists(ruta):
         return None
     
     try:
+        mtime = os.stat(ruta).st_mtime
+        ctime = os.stat(ruta).st_ctime
+        
         h = hashlib.new("SHA256")
         with open(ruta, "rb") as f: # Abrimos el archivo (se cierra al terminar incluso en fallo) y leemos el binario
             
@@ -17,7 +21,11 @@ def calcular_hash(ruta):
             # vacios (esto lo hacemos con b"")
             for bloque in iter(lambda: f.read(4096), b""):
                 h.update(bloque)
-        return h.hexdigest()
+        return {
+            "hash": h.hexdigest(),
+            "mtime": mtime,
+            "ctime": ctime
+        }
     
     except PermissionError:
         return "FALTAN PERMISOS"
@@ -40,12 +48,12 @@ def generar_baseline():
     
     # Para cada archivo nos guardamos el hash asociado y lo metemos en un diccionario
     for ruta in todas_las_rutas:
-        hash_val = calcular_hash(ruta)
+        hash_val = calcular_integridad(ruta)
         # Comprobamos que nos devuelve la función, solo lo guardamos si obtenemos exitosamente el hash
         if hash_val == None:
             print("     [!] El archivo '"+ruta+"' no existe")
             
-        elif hash_val == "FALTAN PERMISOS" or hash_val.startswith("ERROR:"):
+        elif isinstance(hash_val, str) and (hash_val == "FALTAN PERMISOS" or hash_val.startswith("ERROR:")):
             print("     [!] No se ha logrado obtener el archivo de '"+ruta+"' -> "+str(hash_val))
             
         else:
@@ -93,13 +101,13 @@ def verificar_integridad(verbose):
     
     # Obtenemos los hashes igual que en baseline
     for ruta in todas_las_rutas:
-        hash_val = calcular_hash(ruta)
+        hash_val = calcular_integridad(ruta)
         
         # Comprobamos que nos devuelve la función, solo lo guardamos si obtenemos exitosamente el hash
         if hash_val == None:
             print("     [!] El archivo '"+ruta+"' no existe")
             
-        elif hash_val == "FALTAN PERMISOS" or hash_val.startswith("ERROR:"):
+        elif isinstance(hash_val, str) and (hash_val == "FALTAN PERMISOS" or hash_val.startswith("ERROR:")):
             print("     [!] No se ha logrado obtener el archivo de '"+ruta+"' -> "+str(hash_val))
             
         else:
@@ -150,5 +158,6 @@ def verificar_integridad(verbose):
 
 
 def ESCANEO_integridad(verbose):
+    print("\n--- [ FASE 3: AUDITORÍA DE INTEGRIDAD ] ---")
     resultado = verificar_integridad(verbose)
     return resultado
